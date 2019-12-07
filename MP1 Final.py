f7e986f9e848f9c914ff5ce34e118d499c34b3ab
JohnEmmanuelReyes <47804220+JohnEmmanuelReyes@users.noreply.github.com>
2019-12-07 22:46:24 +0800

    Add files via upload

---
+++ MP1 Final.py
@@ -0,0 +1,22 @@
+#Use np.linspace(0,99,100) as n
+def f(n):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    y = np.copy(n)
+    #Numpy version of conditional statements
+    y[y < 10] = y[y < 10]**2 - 7
+    #for the value of n from 10 to 99
+    #It will loop the function until it reaches n less than 10.
+    for x in range(10, n.size):
+        y[x] = y[x - 10]
+    
+    #Plotting Specifications    
+    plt.figure(figsize=(10,7))    
+    plt.suptitle('Piecewise of f(n)')
+    plt.xlabel('n in range of (0,99)')
+    plt.ylabel('f (n)')
+    plt.stem(n,y, linefmt='-.', markerfmt='bo', basefmt= 'r-')
+    plt.show
+
+    return 
